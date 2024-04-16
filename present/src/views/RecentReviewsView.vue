11e4e4c6d9078018d49b370cc9970452b5202518
angelicaw133 <angelicawo426@gmail.com>
2024-04-16 10:07:09 -0400

    d

--- present/src/views/RecentReviewsView.vue
+++ present/src/views/RecentReviewsView.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <form @submit.prevent="onSubmit"></form>
   </div>
 </template>
 
 <script setup>
-import {ref, onBeforeMount} from "vue";
-const restaurants = ref("")
-async function getrestaurant() {
-  let res = await fetch ("https://data.cityofnewyork.us/resource/pitm-atqc.json")
-  let data = await res.json();
-  restaurants.value = data;
-  const test = data.filter((da)=> da.restaurant_name == "Pomp and Circumstance Hospitality")
-  console.log(test)
-}
-onBeforeMount(()=>{
-  getrestaurant();
-})
+// import {ref, onBeforeMount} from "vue";
+// const restaurants = ref("")
+// async function getrestaurant() {
+//   let res = await fetch ("https://data.cityofnewyork.us/resource/pitm-atqc.json")
+//   let data = await res.json();
+//   restaurants.value = data;
+//   const test = data.filter((da)=> da.restaurant_name == "Pomp and Circumstance Hospitality")
+//   console.log(test)
+// }
+// onBeforeMount(()=>{
+//   getrestaurant();
+// })
+let { data: Restaurants, error } = await supabase
+  .from('Restaurants')
+  .select('some_column,other_column')
 </script>
 
