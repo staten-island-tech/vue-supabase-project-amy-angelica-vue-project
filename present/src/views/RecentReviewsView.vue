b83259ce6836e50117584f11eaf0bd619af0d7a2
angelicaw133 <angelicawo426@gmail.com>
2024-04-17 10:11:05 -0400

    l;

--- present/src/views/RecentReviewsView.vue
+++ present/src/views/RecentReviewsView.vue
@@ -4,8 +4,7 @@
 </template>
 
 <script setup>
-// import {ref, onBeforeMount} from "vue";
-// const restaurants = ref("")
+
 // async function getrestaurant() {
 //   let res = await fetch ("https://data.cityofnewyork.us/resource/pitm-atqc.json")
 //   let data = await res.json();
@@ -16,8 +15,11 @@
 // onBeforeMount(()=>{
 //   getrestaurant();
 // })
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://uzufnrmrvcxbarxfvhks.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dWZucm1ydmN4YmFyeGZ2aGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTE5NzcsImV4cCI6MjAyNzcyNzk3N30.-iz6vsmlLmImNAIyNjVGPy-q0dznZNeT9W_sRbEJ2mM')
+
 let { data: Restaurants, error } = await supabase
   .from('Restaurants')
-  .select('some_column,other_column')
+  .select('Legal Business Name')
 </script>
 
