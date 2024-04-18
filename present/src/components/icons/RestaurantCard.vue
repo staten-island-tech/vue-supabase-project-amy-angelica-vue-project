bd58a35d8f0e47798bcb74aa11ff4300a967c27f
angelicaw133 <angelicawo426@gmail.com>
2024-04-18 10:14:51 -0400

    l

---
+++ present/src/components/icons/RestaurantCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <h2>Unique ID: {{ restaurant.objectid }}</h2>
+        <h2>name: {{ restaurant.restaurant_name }}</h2>
+ </div>
+</template>
+
+<script setup>
+const props = defineProps({
+   restaurant: Object
+})
+</script>
+
+<style scoped>
+.card {
+   font-size: 0.5rem;
+   background-color: white;
+   padding: 2%;
+   margin: 2rem 2rem;
+   border: 3px solid black;
+   width: 200px;
+   height: 45vh;
+ } 
+</style>
--- present/src/views/RecentReviewsView.vue
+++ present/src/views/RecentReviewsView.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class="container">
+      <RestaurantCard v-for="restaurant in restaurants" :key="restaurant.objectid" :restaurant="restaurant"/>
+    </div>
   </div>
 </template>
 
@@ -16,10 +19,17 @@
 //   getrestaurant();
 // })
 import { createClient } from '@supabase/supabase-js'
+import { onBeforeMount } from 'vue';
 const supabase = createClient('https://uzufnrmrvcxbarxfvhks.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dWZucm1ydmN4YmFyeGZ2aGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTE5NzcsImV4cCI6MjAyNzcyNzk3N30.-iz6vsmlLmImNAIyNjVGPy-q0dznZNeT9W_sRbEJ2mM')
-
-let { data: Restaurants, error } = await supabase
+async function getrestaurant() {
+let { data: restaurant, error } = await supabase
   .from('Restaurants')
-  .select('Legal Business Name')
+  .select('objectid',
+  'restaurant_name')
+  console.log(restaurant)}
+  onBeforeMount(()=>{
+    getrestaurant();
+  })
+  import RestaurantCard from '@/components/icons/RestaurantCard.vue';
 </script>
 
