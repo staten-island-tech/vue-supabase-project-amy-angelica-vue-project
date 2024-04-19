c5404d717121e5f3d155f357e2c4de76fa73caae
angelicaw133 <angelicawo426@gmail.com>
2024-04-19 10:14:50 -0400

    d

--- present/src/components/icons/RestaurantCard.vue
+++ present/src/components/icons/RestaurantCard.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="card">
-        <h2>Unique ID: {{ restaurant.objectid }}</h2>
-        <h2>name: {{ restaurant.restaurant_name }}</h2>
+        <h1>{{ restaurant.restaurant_name }}</h1>
+        <h2>{{ restaurant.business_address + ", " + restaurant.postcode }}</h2>
+        <h2>Sidewalk Seating: {{ restaurant.approved_sidewalk_seating }}</h2>
+         <h2>Road Seating: {{ restaurant.approved_road_seating }}</h2>
+         
+
  </div>
 </template>
 
@@ -20,5 +24,9 @@ const props = defineProps({
    border: 3px solid black;
    width: 200px;
    height: 45vh;
+
+ }
+ h1, h2, h3 {
+   background-color: white;
  } 
 </style>
--- present/src/views/RecentReviewsView.vue
+++ present/src/views/RecentReviewsView.vue
@@ -18,18 +18,34 @@
 // onBeforeMount(()=>{
 //   getrestaurant();
 // })
+
 import { createClient } from '@supabase/supabase-js'
-import { onBeforeMount } from 'vue';
+import { ref, onBeforeMount } from 'vue';
 const supabase = createClient('https://uzufnrmrvcxbarxfvhks.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dWZucm1ydmN4YmFyeGZ2aGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTE5NzcsImV4cCI6MjAyNzcyNzk3N30.-iz6vsmlLmImNAIyNjVGPy-q0dznZNeT9W_sRbEJ2mM')
+const restaurants = ref("")
 async function getrestaurant() {
 let { data: restaurant, error } = await supabase
   .from('Restaurants')
-  .select('objectid',
-  'restaurant_name')
-  console.log(restaurant)}
+  .select('*')
+  restaurants.value = restaurant}
   onBeforeMount(()=>{
     getrestaurant();
   })
   import RestaurantCard from '@/components/icons/RestaurantCard.vue';
 </script>
+<style  scoped>
+
+  .container {
+    display: flex;
+  align-items: center;
+  text-align: center;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  
+}
+.title{
+  font-size: 50px;
+  text-align: center;
+}
+</style>
 
