6dac80f598e9da60bb5c14df76f3f96a6e5e62f4
angelicaw133 <angelicawo426@gmail.com>
2024-05-13 10:14:38 -0400

    Update RestaurantData.vue

--- present/src/views/RestaurantData.vue
+++ present/src/views/RestaurantData.vue
@@ -14,10 +14,10 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dWZucm1ydmN4YmFyeGZ2aGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTE5NzcsImV4cCI6MjAyNzcyNzk3N30.-iz6vsmlLmImNAIyNjVGPy-q0dznZNeT9W_sRbEJ2mM'
 )
 let selected_rest = ref("");
-console.log(selected_rest)
 async function get_s_restaurant() {
-  let { data: restaurant, error } = await supabase.from('Restaurants').select(`${route.params.restaurant_id}`)
+  let { data: restaurant, error } = await supabase.from('Restaurants').select(`*`)
   selected_rest.value = restaurant;
+  console.log(restaurant)
 }
 onMounted(() => {
   get_s_restaurant();
