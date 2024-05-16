dea9e4b535260590e39c27824b9737ce16f9d3e8
angelicaw133 <angelicawo426@gmail.com>
2024-05-16 10:13:21 -0400

    Update RestaurantData.vue

--- present/src/views/RestaurantData.vue
+++ present/src/views/RestaurantData.vue
@@ -1,11 +1,12 @@
 <template>
-  <h1>hi</h1> 
-  <h1>{{ restaurant.restaurant_id }}</h1>
-  <h1>{{ restaurant.restaurant_name }}</h1>
+  <h2>sdfg</h2>
+  <h1>{{ selected_rest[0].restaurant_name }}</h1>
+  <h1>{{ selected_rest[0].restaurant_name }}</h1>
+  
 </template>
 
-<script>
-import { ref, onMounted } from "vue";
+<script setup>
+import { ref, onBeforeMount } from "vue";
 import { useRoute } from "vue-router";
 const route = useRoute();
 import { createClient } from '@supabase/supabase-js'
@@ -13,13 +14,21 @@ const supabase = createClient(
   'https://uzufnrmrvcxbarxfvhks.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dWZucm1ydmN4YmFyeGZ2aGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTE5NzcsImV4cCI6MjAyNzcyNzk3N30.-iz6vsmlLmImNAIyNjVGPy-q0dznZNeT9W_sRbEJ2mM'
 )
-let selected_rest = ref("");
+let selected_rest = ref('');
 async function get_s_restaurant() {
-  let { data: restaurant, error } = await supabase.from('Restaurants').select(`*`)
+  try {
+    let { data: restaurant, error } = await supabase.from('Restaurants').select('*').eq('restaurant_id', `${route.params.restaurant_id}`);
   selected_rest.value = restaurant;
   console.log(restaurant)
+  if(error){
+    throw new Error(error)
+  }
+  } catch (error) {
+    console.log(error)
+  }
+  
 }
-onMounted(() => {
+onBeforeMount(() => {
   get_s_restaurant();
 })
 
