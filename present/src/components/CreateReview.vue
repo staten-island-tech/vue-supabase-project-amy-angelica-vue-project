67e8ad2b56f56dfc8e8e88a76ccd3ec031f33d2d
angelicaw133 <angelicawo426@gmail.com>
2024-05-24 10:14:51 -0400

    e

--- present/src/components/CreateReview.vue
+++ present/src/components/CreateReview.vue
@@ -1,15 +1,31 @@
 <template>
   <h1>Write A Review!:</h1>
 
-  <starrating />
-  <button @click.once="post_it()">Post Review</button>
+  <div>
+    <div v-if="counter == 0">
+      <h2>Give a rating!</h2>
+    </div>
+    <div v-else>
+      <h2>Rating: {{ counter }}</h2>
+    </div>
+    <button class="1" @click="counter = 1">1</button>
+    <button class="2" @click="counter = 2">2</button>
+    <button class="3" @click="counter = 3">3</button>
+    <button class="4" @click="counter = 4">4</button>
+    <button class="5" @click="counter = 5">5</button>
+  </div>
+  <button @click.once="post_it(counter)">Post Review</button>
 </template>
 
 <script setup>
 import { supabase } from './supabaseClient'
-import starrating from './icons/starrating.vue'
+import { ref } from 'vue'
+const counter = ref(0)
+if (counter !== 0) {
+  let rating = counter
+}
 
-async function post_it() {
+async function post_it(rating) {
   try {
     const { error } = await supabase.from('Posts')
     .insert({ 
