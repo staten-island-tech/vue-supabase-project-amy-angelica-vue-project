f66762166d6fb0f446f1abbedd3d0d0d8e2e9cf0
angelicaw133 <angelicawo426@gmail.com>
2024-05-29 10:14:09 -0400

    Update storecreate.js

--- present/src/stores/storecreate.js
+++ present/src/stores/storecreate.js
@@ -1,4 +1,10 @@
-import { defineStore } from 'pinia'
+import { createApp } from 'vue'
+import { createPinia, defineStore } from 'pinia'
+import { App } from 'vue'
+const pinia = createPinia()
+const app = createApp(APP)
+app.use(pinia)
+app.mount('#app')
 export const useStore = defineStore('StoreCreated',{
     state: () => {
         return {
