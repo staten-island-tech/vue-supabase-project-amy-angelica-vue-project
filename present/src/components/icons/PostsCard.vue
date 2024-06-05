559a4a7aaf9a9f002a2ed6f51a9a65a98ae92989
angelicaw133 <angelicawo426@gmail.com>
2024-06-05 10:14:44 -0400

    d

--- present/src/components/icons/PostsCard.vue
+++ present/src/components/icons/PostsCard.vue
@@ -1,10 +1,10 @@
 <template> 
+
 <div class="card">      
 <h2>{{ post.user_id }}</h2>
   <h3>Created at:{{ post.created_at }}</h3>
         <h2>Rating: {{ post.rating }}</h2>
        <h2>Review: {{ post.review }}</h2> 
-       <h2>reply: {{ replies.id }}</h2>
 <likebutton/>
 <!-- <input v-model="message" placeholder="edit me" />
 <button @click="reply_to(message)">Post Reply</button> -->
@@ -12,31 +12,13 @@
 </template>
 
 <script setup>
-import { supabase } from '../supabaseClient';
-import { ref, onBeforeMount } from 'vue';
+
 import likebutton from './likebutton.vue';
 const props = defineProps({
    post: Object,
    postid: Number 
 })
-let replies = ref('');
-async function get_replies() {
-  try {
-    let { data: reply, error } = await supabase.from('Replies').select('*');
-  replies.value = reply;
-    console.log(reply)
-  if(error){
-    throw new Error(error)
-  }
-  } catch (error) {
-    console.log(error)
-  }
-  
-}
-onBeforeMount(() => {
-  get_replies();
 
-});
 
 // function reply_to(message){
 
--- present/src/components/icons/likebutton.vue
+++ present/src/components/icons/likebutton.vue
@@ -4,9 +4,27 @@
   </template>
   
   <script setup>
+  import { supabase } from '../supabaseClient';
  import { ref } from 'vue'
+ const counter = ref(0)
 
-const counter = ref(0)
+    async function like_it(counter) {
+  try {
+    const { error } = await supabase.from('Posts')
+    .insert({ 
+  
+      upvote: counter,
+
+    })
+    console.log('hi')
+    if (error) {
+      throw new Error(error)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+like_it(counter)
   </script>
   <style>
  
